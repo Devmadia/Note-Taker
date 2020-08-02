515f83224800424bf0b166f7183cf8e1ff7fd30a
Devmadia <agathaphan@icloud.com>
2020-08-02 15:25:16 -0500

    added post function, require notes db

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,8 +1,15 @@
 const router = require('express').Router();
 
+const notes = require('../../db/db.json');
+
 router.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+router.post('/api/notes', (req,res) => {
+    // creates a note
+    const note = createNote(req.body, notes);
+    res.json(note);
+})
 
 module.exports = router;
