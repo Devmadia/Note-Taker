e52a32bb97311d0d07a97fb254d26449348048a7
Devmadia <agathaphan@icloud.com>
2020-08-02 14:00:15 -0500

    initialized Express and Jest for later testing, set up route folders, created basic middleware code and introduced routes for notes/index

---
+++ routes/htmlRoutes/index.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const router = require('express').Router();
+
+// HTML routes to return the index.html file
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+// HTML routes to return the notes.html file
+router.get('/notes', (req, res) => {
+res.sendFile(path.join(__dirname, '../../public/notes.html'));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express'); // require Express.js
+
+// routes
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
+// set port for Heroku app to run through 3001 instead of default 80
+const PORT = process.env.PORT || 3001;
+
+// Setting up the server: first step to instantiate the server
+const app = express();
+
+// middleware and parsing
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+
+// parse incoming JSON data
+app.use(express.json());
+
+// direction to access public folder data
+app.use(express.static('public'));
+
+// Setting up the server: listen for requests
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+  });
