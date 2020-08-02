a4e513ca6359a063fa438553a9b5f66284e74a10
Devmadia <agathaphan@icloud.com>
2020-08-02 16:47:40 -0500

    router issue causing notes page not to load until route order of process fixed

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -3,14 +3,16 @@ const router = require('express').Router();
 const { makeNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 
+// GET request
 router.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+// POST request
 router.post('/api/notes', (req,res) => {
     // creates a note
-    const note = createNote(req.body, notes);
-    res.json(note);
+    const note = makeNote(req.body, notes);
+    res.json(notes);
 })
 
 module.exports = router;
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const router = require('express').Router();
 
-// HTML routes to return the index.html file
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../public/index.html'));
-});
-
 // HTML routes to return the notes.html file
 router.get('/notes', (req, res) => {
 res.sendFile(path.join(__dirname, '../../public/notes.html'));
 });
+
+// HTML routes to return the index.html file
+
+router.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/index.html'));
+});
+
+
 
 module.exports = router;
