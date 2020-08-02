4a16423dab4cafca10e121dbde3c9e87e1abdcf9
Devmadia <agathaphan@icloud.com>
2020-08-02 16:18:33 -0500

    added function makeNotes route

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { makeNote } = require('../../lib/notes');
 const notes = require('../../db/db.json');
 
 router.get('/api/notes', (req, res) => {
