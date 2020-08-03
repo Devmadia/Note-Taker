e7f8aaf95868f63e9cb55d8f328588a5c1128026
Devmadia <agathaphan@icloud.com>
2020-08-02 21:43:49 -0500

    minor changes to comments and arrangement

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -5,12 +5,12 @@ let notes = require('../../db/db.json');
 
 // API routes
 // GET request
-router.get('/notes', (req,res) => {
+router.get('/notes', (req, res) => {
     res.json(notes);
 });
 
 // POST request to make a note
-router.post('/notes', (req,res) => {
+router.post('/notes', (req, res) => {
     // creates a note in the db.json file
     const newNote = req.body;  // matching call passed through from lib/notes
     saveNote(newNote);
@@ -18,7 +18,7 @@ router.post('/notes', (req,res) => {
 });
 
 // route request to delete a note
-router.delete('/notes:id', (req,res) => {
+router.delete('/notes:id', (req, res) => {
     //uses the ID to delete a note
     notes = deleteNote(req.params.id);
     res.json(notes);
